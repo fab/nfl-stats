53e2969d2bf43fbd67aad877e27df0b98a2f4caf
Fab Mackojc <fab.mackojc@gmail.com>
2015-08-12 22:47:30 -0300

    Nicely refactored

--- main.js
+++ main.js
@@ -1,55 +1,101 @@
-$(document).ready(function() {
-  // set window height
-  var windowHeight = $(window).height()
-  $('body').height(windowHeight)
+function setWindowHeight() {
+  var windowHeight = $(window).height();
+  $('body').height(windowHeight);
+}
 
-  // make the dropdown a select element and update stats if team is changed
+function setEventListenerOnSelect() {
   $('select').select2({ width: '220px' }).on('change', function(e) {
-    var new_team_id = $(this).val()
-    var filename = 'data/' + new_team_id + '.json'
+    var newTeamAbbr = $(this).val(); // eg. STL
 
-    // update url
-    window.history.pushState('', 'NFL Team Stats', '/#/' + new_team_id)
-
-    $.getJSON(filename, function(data) {
-      var team_stat_cells = $('.stat')
-      for (var i = 0; i < data.team_stats.length; i++) {
-        var team_stat = data.team_stats[i];
-        var team_stat_cell = $(team_stat_cells[i]);
-        team_stat_cell.find('.value').text(team_stat.value);
-        team_stat_cell.find('strong').text(team_stat.rank);
-
-        // set rank color code
-        var rank_value = parseInt(team_stat.rank);
-        var rank_color_code = null;
-        if (rank_value <= 5) {
-          rank_color_code = 'high';
-        } else if ((rank_value <= 27) && (rank_value >= 23)) {
-          rank_color_code = 'medium';
-        } else if (rank_value >= 28) {
-          rank_color_code = 'low';
-        }
-        team_stat_cell.find('.rank').children().first().removeClass().addClass(rank_color_code);
-      }
-    });
+    updateURL(newTeamAbbr);
+    fetchDataAndUpdateDOM(newTeamAbbr);
   });
+}
 
-  // on initial request load stats for team name in URL
-  var team_name_in_url = location.hash.slice(2).toUpperCase() || 'ARI';
-  $("select").val(team_name_in_url).trigger("change");
+function updateURL(teamAbbr) {
+  window.history.pushState('', 'NFL Team Stats', '/#/' + teamAbbr);
+}
+
+function filename(teamAbbr) {
+  return 'data/' + teamAbbr + '.json';
+}
+
+function teamAbbrInURL() {
+  return location.hash.slice(2).toUpperCase();
+}
+
+function fetchDataAndUpdateDOM(newTeamAbbr) {
+  $.getJSON(filename(newTeamAbbr), function(data) {
+    updateAllTeamStatDivs(data)
+  });
+}
+
+function updateAllTeamStatDivs(data) {
+  var teamStatDivs = $('.stat')
+  for (var i = 0; i < data.team_stats.length; i++) {
+    var teamStat = data.team_stats[i];
+    var teamStatDiv = $(teamStatDivs[i]);
+
+    updateTeamStatDiv(teamStat, teamStatDiv);
+  }
+}
+
+function updateTeamStatDiv(teamStat, teamStatDiv) {
+  var statValue = teamStat.value;
+  var rankText = teamStat.rank;
+  updateValue(statValue, teamStatDiv);
+  updateRank(rankText, teamStatDiv);
+}
+
+function updateValue(statValue, teamStatDiv) {
+  teamStatDiv.find('.value').text(statValue);
+}
+
+function updateRank(rankText, teamStatDiv) {
+  var rankValue = parseInt(rankText);
+  var rankCategory = categoriseRank(rankValue);
+
+  teamStatDiv.find('strong').text(rankText);
+  teamStatDiv.find('.rank').children().first().removeClass().addClass(rankCategory);
+}
+
+function categoriseRank(rankValue) {
+  if (rankValue <= 5) {
+    return 'high';
+  } else if ((rankValue <= 27) && (rankValue >= 23)) {
+    return 'medium';
+  } else if (rankValue >= 28) {
+    return 'low';
+  } else {
+    return ''
+  }
+}
+
+function triggerSelectChange(teamAbbr) {
+  $("select").val(teamAbbr).trigger("change");
+}
+
+function loadStatsForTeamInURL() {
+  var teamAbbr = teamAbbrInURL() || 'ARI';
+  triggerSelectChange(teamAbbr);
+}
+
+function locationHashChanged() {
+  var newTeamAbbr = teamAbbrInURL() || 'ARI'; // default to ARI if no abbr
+
+  // check if there is team data for that abbr, if not then default to ARI
+  $.ajax({
+    url: filename(newTeamAbbr),
+    success: function() { triggerSelectChange(newTeamAbbr); },
+    error: function() { triggerSelectChange('ARI'); }
+  });
+}
+
+$(document).ready(function() {
+  setWindowHeight();
+  setEventListenerOnSelect();
+  loadStatsForTeamInURL();
 
   // if the user changes the URL then we update the stats for the desired team
   window.onhashchange = locationHashChanged;
-
-  function locationHashChanged() {
-    var team_name_in_url = location.hash.slice(2).toUpperCase() || 'ARI';
-    var filename = 'data/' + team_name_in_url + '.json'
-
-    // check if there is a team for that name
-    $.ajax({
-      url: filename,
-      success: function() { $("select").val(team_name_in_url).trigger("change"); },
-      error: function() { $("select").val('ARI').trigger("change"); }
-    });
-  }
 });
